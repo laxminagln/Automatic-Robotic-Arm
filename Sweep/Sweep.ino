0b9df84b954d37ee941aaa8afb296c4195375f5d
nagln <laxminagln@gmail.com>
2021-10-23 16:12:36 +0530

    Update Sweep.ino

--- Sweep/Sweep.ino
+++ Sweep/Sweep.ino
@@ -1,20 +1,8 @@
-
-
-
-
-
-
-
-
-
-
-
 #include <Servo.h>
 Servo servo1, servo2, servo3, servo4, servo5, servo6;
 int pos = 0;    
 const int pingPin = 7; 
 const int echoPin = 4; 
-
 
 void setup() {
   Serial.begin(9600);
@@ -27,7 +15,6 @@ void setup() {
   servo5.attach(10);
   servo6.attach(11);
 }
-
 
 void loop() {
   servo1.write(0);
@@ -47,6 +34,7 @@ void loop() {
     delay(1000);                       
   }
 }
+
 void ultrasonic() 
 {
    int duration, distance;
@@ -86,76 +74,3 @@ void servorot(int d)
     delay(2000);
     exit(0);
   }
-  /*else if(d>6 && d<=9)
-  {
-    Serial.println("d>6 && d<=9");
-    servo2.write(30);
-    delay(200);
-    servo3.write(90);
-    delay(200);
-    servo5.write(115);
-    delay(200);
-  }
-  else if(d>9 && d<=12)
-  {
-    Serial.println("d>9 && d<=12");
-    servo2.write(40);
-    delay(200);
-    servo3.write(100);
-    delay(200);
-    servo5.write(120);
-    delay(200);
-  }
-  else if(d>12 && d<=15)
-  {
-    Serial.println("d>12 && d<=15");
-    servo2.write(50);
-    delay(200);
-    servo3.write(110);
-    delay(200);
-    servo5.write(125);
-    delay(200);
-  }
-  else if(d>15 && d<=18)
-  {
-    Serial.println("d>15 && d<=18");
-    servo2.write(60);
-    delay(200);
-    servo3.write(120);
-    delay(200);
-    servo5.write(130);
-    delay(200);
-  }*/
-}
-/*
-void setup() {
-  Serial.begin(9600);
-  pinMode(pingPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  servo1.attach(3);  
-  servo2.attach(5);
-  servo3.attach(6);
-  servo4.attach(9);
-  servo5.attach(10);
-  servo6.attach(11);
-}
-
-void loop() {
-  servo1.write(0);
-  servo2.write(0);
-  servo3.write(180);
-  servo4.write(0);
-  servo5.write(180);
-  servo6.write(0);
-  for (pos = 0; pos <= 180; pos += 1) { 
-    servo1.write(pos);     
-    ultrasonic();         
-    delay(100);                       
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { 
-    servo1.write(pos); 
-    ultrasonic();               
-    delay(100);                       
-  }
-}
-*/
